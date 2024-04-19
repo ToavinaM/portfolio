0d78da7c0679d028797b35fd96a9ebe1b09d99b8
toavinaDev <157911400+toavinaDev@users.noreply.github.com>
2024-04-19 15:58:07 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormParticipation from './form/formParticipation';
+
+function App() {
+  return (
+    <div className="App">
+    <FormParticipation></FormParticipation>
+    </div>
+  );
+}
+
+export default App;
