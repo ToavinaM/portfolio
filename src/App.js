b799b8f455b74fd718ce78098b7441639bfcd07c
toavinaDev <135843363+sharatprox@users.noreply.github.com>
2024-04-24 22:16:41 +0300

    add banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Banner from './banner/Banner';
 import FormParticipation from './form/formParticipation';
 
 function App() {
   return (
     <div className="App">
-    <FormParticipation></FormParticipation>
+      {/* <FormParticipation></FormParticipation> */}
+      <Banner></Banner>
     </div>
   );
 }
---
+++ src/banner/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Banner() {
+    return (
+        <React.Fragment>
+            <img src="images/banner.png" width="100%" height="auto" />
+        </React.Fragment>
+    )
+}
