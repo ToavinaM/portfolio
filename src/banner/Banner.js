e30a68d44768a1ae6e5d3a26e00078f6d9b26b46
toavinaDev <157911400+toavinaDev@users.noreply.github.com>
2024-04-30 14:57:15 +0300

    [FIX] trigger build

--- src/banner/Banner.js
+++ src/banner/Banner.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Banner() {
     return (
         <>
-            <img alt="developpeur" src="images/banner.png" width="100%" height="100%" />
+            <img alt="developpeur" src="images/toavina.png" width="100%" height="100%" />
             {/* <img alt="tset" src="./test.png" width="100%" height="100%" /> */}
 
         </>
